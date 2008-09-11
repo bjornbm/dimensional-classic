19998403e65ed0ee5e46a2c6b783375471b5849a
Bjorn Buckwalter <bjorn.buckwalter@gmail.com>
2008-09-11 00:33:41 +0000

    Creative comments caused Haddock parse error (not that Haddock is very useful for .lhs anyway)

--- Numeric/Units/Dimensional/Prelude.hs
+++ Numeric/Units/Dimensional/Prelude.hs
@@ -16,7 +16,7 @@ import Numeric.Units.Dimensional.SIUnits
 
 import Numeric.NumType 
     ( neg5, neg4, neg3, neg2, neg1, zero, pos1, pos2, pos3, pos4, pos5
-    ) -- ^Used in exponents.
+    )  -- Used in exponents.
 
 import Prelude hiding
     ( (+), (-), (*), (/), (^), (**)
@@ -24,4 +24,4 @@ import Prelude hiding
     , sin, cos, tan, asin, acos, atan, atan2
     , sinh, cosh, tanh, asinh, acosh, atanh
     , sum
-    ) -- ^Hide definitions overridden by 'Numeric.Dimensional'.
+    )  -- Hide definitions overridden by 'Numeric.Dimensional'.
