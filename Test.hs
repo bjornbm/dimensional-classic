e43fd35045eb1b5f5bc81e840e6c5017db3abd0f
Bjorn Buckwalter <bjorn.buckwalter@gmail.com>
2009-06-06 03:24:22 +0000

    Removed NumType, added numtype dep, upped version.

--- Test.hs
+++ Test.hs
@@ -1,12 +1,8 @@
-
-import qualified Numeric.NumTypeTests
 import qualified Numeric.Units.Dimensional.Test
 import qualified Numeric.Units.Dimensional.QuantitiesTest
 import qualified Numeric.Units.Dimensional.ExtensibleTest
 
 main = do
-  Numeric.NumTypeTests.main
   Numeric.Units.Dimensional.Test.main
   Numeric.Units.Dimensional.QuantitiesTest.main
   Numeric.Units.Dimensional.ExtensibleTest.main
-
